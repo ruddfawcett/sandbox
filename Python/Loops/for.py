7e5b432ada36297994847b98d3a0c62bc5539cd5
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-11-29 16:36:44 -0500

    Added Python files

---
+++ Python/Loops/for.py
@@ -0,0 +1,6 @@
+for i in range(0, 9):
+    print(i)
+
+    i += 1
+
+print("Done.")
---
+++ Python/Loops/while.py
@@ -0,0 +1,11 @@
+maybe = True
+i = 0
+
+while maybe:
+    print(i)
+
+    if i == 9:
+        maybe = False
+        print("Done.")
+
+    i += 1
